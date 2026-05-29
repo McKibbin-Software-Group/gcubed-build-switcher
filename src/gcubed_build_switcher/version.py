ab4946ea6500ea76bb163715921efa73ca67c287
Ash <aaschoff@gcubed.com>
2026-05-29 13:56:29 +1000

    Patch release to sync with current releases

--- src/gcubed_build_switcher/version.py
+++ src/gcubed_build_switcher/version.py
@@ -10,7 +10,7 @@ except ImportError:  # pragma: no cover - Python < 3.8 compatibility path.
 
 
 PACKAGE_NAME = "gcubed-build-switcher"
-FALLBACK_VERSION = "1.2.3"
+FALLBACK_VERSION = "2.1.5"
 
 
 def get_package_version():
