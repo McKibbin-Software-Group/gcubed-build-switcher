d616dde3c157f09e2e50911ab67df44a23172160
Ash <aaschoff@gcubed.com>
2026-05-29 16:19:07 +1000

    Release gcubed-build-switcher 2.2.2

--- src/gcubed_build_switcher/version.py
+++ src/gcubed_build_switcher/version.py
@@ -10,7 +10,7 @@ except ImportError:  # pragma: no cover - Python < 3.8 compatibility path.
 
 
 PACKAGE_NAME = "gcubed-build-switcher"
-FALLBACK_VERSION = "2.2.1"
+FALLBACK_VERSION = "2.2.2"
 
 
 def get_package_version():
